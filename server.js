c38e2b6c46faefd5ae35c43d8f5a4b6e455290fc
Thomas Hsu <thomhsu@gmail.com>
2019-03-15 14:51:11 -0700

    Add body parser

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const parser = require('body-parser');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
